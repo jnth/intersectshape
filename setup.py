7bcfa9205451c5136d3503c90cb767124eb367cd
Jonathan Virga <jonathan.virga@gmail.com>
2017-03-30 23:18:41 +0200

    Replace Markdown documentation to reStructuredText format.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='intersectshape',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     author='Jonathan Virga',
     author_email='jonathan.virga@gmail.com',
     description='Reverse geocoding from a shapefile of polygons.',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=["numpy", "shapely", "pyshp"],
     include_package_data=True,
     url="https://github.com/jnth/intersectshape",
